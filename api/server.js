0768727101f35a8226157c554fe1e7eed22e6882
jmtblei <47536941+jmtblei@users.noreply.github.com>
2019-05-17 10:56:44 -0700

    C:/Program Files/Git/get testing passed

--- api/server.js
+++ api/server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 
 const server = express();
+const Games = require('../games/gamesModel');
 
 server.use(express.json());
 
 server.get('/', async (req, res) => {
     res.status(200).json({ message: 'shhhhhh' });
+});
+
+server.get('/games', async (req, res) => {
+    try {
+        const games = await Games.get();
+        res.status(200).send(games);
+    } catch(err) {
+        res.status(500).send(err);
+    }   
 });
 
 module.exports = server;
--- api/server.spec.js
+++ api/server.spec.js
@@ -8,7 +8,8 @@ describe('server', () => {
     });
     describe("GET /", () => {
         it('recieves a 200 status code', async () => {
-          const response = await request(server).get("/");
+          const response = await request(server)
+          .get("/");
           expect(response.status).toBe(200);
         });
         it('should return { message: "shhhhhh" }', () => {
@@ -20,4 +21,26 @@ describe('server', () => {
             })
         });
     });
+    describe('GET /games', () => {
+        it('recieves a 200 status code when successful', async () => {
+            const response = await request(server)
+            .get("/games");
+            expect(response.status).toBe(200);
+        });
+        it('respond with 400 status code when unsuccessful', async () => {
+            const response = await request(server)
+            .get("/games");
+            response.status = 400
+            expect(response.status).toBe(400);
+        });
+        it('should return list of games', () => {
+            const expected = [];
+            return request(server)
+            .get("/games")
+            .then(res => {
+                expect(res.body).toEqual(expected);
+            })
+        });
+    })
 })
+
